0de129b5170c1c6c81d41d8e3a640a972c6bbfdb
Ulises Farias <Ulisesfarias1905@hotmail.com>
2024-04-28 18:20:54 -0300

    Tp terminado

--- frontend/js/favoritas.js
+++ frontend/js/favoritas.js
@@ -8,43 +8,38 @@ window.onload = async () => {
 
   // Aqui debemos agregar nuestro fetch
 
-  try {
-    const response = await fetch(`${URL_BASE}movies/${id}`);
-    const result = await response.json()
-    const {data,meta} = result; 
+    try {
+      const response = await fetch('http://localhost:3031/api/movies');
+      const result = await response.json();
+      const { meta, data } = result;
 
-    data.forEach((movie) => {
-      if (favorites.includes(movie.id)) {
-        const card = document.createElement("div");
-    card.setAttribute("class", "card");
+      data.forEach((movie) => {
+        if (favorites.includes(movie.id)) {
+          const card = document.createElement("div");
+      card.setAttribute("class", "card");
 
-    const h1 = document.createElement("h1");
-    h1.textContent = movie.title;
+      const h1 = document.createElement("h1");
+      h1.textContent = movie.title;
 
-    const p = document.createElement("p");
-    p.textContent = `Rating: ${movie.rating}`;
+      const p = document.createElement("p");
+      p.textContent = `Rating: ${movie.rating}`;
 
-    const duracion = document.createElement("p");
-    duracion.textContent = `Duración: ${movie.length}`;
+      const duracion = document.createElement("p");
+      duracion.textContent = `Duración: ${movie.length}`;
 
-    container.appendChild(card);
-    card.appendChild(h1);
-    card.appendChild(p);
-    if (movie.genre !== null) {
-      const genero = document.createElement("p");
-      genero.textContent = `Genero: ${movie.genre.name}`;
-      card.appendChild(genero);
-    }
-    card.appendChild(duracion);
+      container.appendChild(card);
+      card.appendChild(h1);
+      card.appendChild(p);
+      if (movie.genre !== null) {
+        const genero = document.createElement("p");
+        genero.textContent = `Genero: ${movie.genre.name}`;
+        card.appendChild(genero);
       }
-    
-  });
-  } catch (error) {
-    console.log(error);
-  }
-
-   
-
-   
-  
-};
+      card.appendChild(duracion);
+        }
+      
+    });
+    } catch (error) {
+      console.log(error);
+    } 
+  };
